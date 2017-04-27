6ac4444bb1b3cfa9b607b05a12343cdddd77e828
Carlos Hackenbruch <charliee21_12@hotmail.com>
2017-04-27 19:19:18 -0300

    First Examples

---
+++ ForCeibal/ForCeibal/Iteraciones.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public static class Iteraciones
+    {
+
+        public static void Fibonacci()
+        {
+            Console.WriteLine("Bienvenido a la Secuencia de Fibonacci!");
+            Console.Write("Ingrese la posicion deseada: ");
+            int posicion = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Posicion 0: " + 0);
+            Console.WriteLine("Posicion 1: " + 1);
+
+            int primer = 0;
+            int segundo = 1;
+            int suma = 1;
+
+            for (int i = 2; i < posicion; i++)
+            {
+                suma = primer + segundo;
+                Console.WriteLine("Posicion " + i + ": " + suma);
+                primer = segundo;
+                segundo = suma;
+            }
+        }
+        
+        public static void PiramideEstrellas()
+        {
+            Console.WriteLine("Aqui tienes una piramide de ejemplo.");
+            Console.WriteLine();
+            for (int i = 1;i<8;i++)
+            {
+                Console.Write(new string(' ',8-i));
+                Console.Write(String.Concat(Enumerable.Repeat(" *", i)));
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+
+        public static void RomboLoco()
+        {
+            Console.WriteLine("Aqui tienes una piramide de ejemplo.");
+            Console.WriteLine();
+            for (int i = 1; i < 8; i++)
+            {
+                Console.Write(new string(' ', 8 - i));
+                Console.Write(String.Concat(Enumerable.Repeat(" *", i)));
+                Console.WriteLine();
+            }
+
+
+
+            for (int i = 8; i > 0; i--)
+            {
+                Console.Write(new string(' ', 8 - i));
+                Console.Write(String.Concat(Enumerable.Repeat(" *", i)));
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+        }
+
+        public static void SumaHastaN()
+        {
+            Console.WriteLine("Bienvenido a la Seccion de sumas de numeros!");
+            Console.Write("Ingrese un numero entre 1 y 100: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero >= 1 && numero <= 100)
+            {
+                int suma = 0;
+                for (int i = 1; i <= numero; i++)
+                {
+                    suma = suma + i; //+=
+                }
+                Console.WriteLine("La suma da " + suma);
+            }
+            else
+            {
+                Console.WriteLine("Numero no valido");
+            }
+
+            /*Solucion ideal:
+             Console.WriteLine("Ingrese un numero entre 1 y 100");
+             int numero = Convert.ToInt32(Console.ReadLine());
+
+             if (numero >= 1 && numero <= 100)
+             {
+                 int suma = numero * (numero - 1) / 2;
+                 Console.WriteLine("La suma da " + suma);
+             }
+             else
+             {
+                 Console.WriteLine("Numero no valido");
+             }
+            */
+        }
+    }
+}
---
+++ ForCeibal/ForCeibal/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    class Program
+    {
+        public delegate void Exercise();
+        static void Main(string[] args)
+        {
+            Menu(LoadExercises());
+        }
+
+        private static List<Exercise> LoadExercises()
+        {
+            return new List<Exercise>() {
+                Iteraciones.Fibonacci,
+                Iteraciones.SumaHastaN,
+                Iteraciones.PiramideEstrellas,
+                Iteraciones.RomboLoco,
+            };
+        }
+
+        private static void Menu(List<Exercise> exercises)
+        {
+            string option = "";
+            bool leaving = false;
+            try
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Bienvenido a los ejemplos.");
+                    exercises.ForEach(x => Console.WriteLine(exercises.IndexOf(x) + "-> " + x.Method.Name));
+                    Console.WriteLine("x-> Salir");
+                    Console.WriteLine();
+                    Console.Write("Por favor seleccione una opcion: ");
+                    option = Console.ReadLine();
+                    int aux = Convert.ToInt16(option);
+                    Console.WriteLine();
+                    if (aux > -1 && aux < exercises.Count)
+                    {
+                        exercises[aux]();
+                    }
+                    leaving = option.ToString().ToLower().Equals("x");
+                    if (!leaving)
+                    {
+                        Console.WriteLine("Presione cualquier tecla para refrescar el menu");
+                        Console.ReadKey();
+                    }
+                    Console.WriteLine();
+                }
+                while (!leaving);
+            }
+            catch (Exception)
+            {
+                if (!option.ToString().ToLower().Equals("x")) Menu(exercises); //!!
+            }
+        }
+    }
+}
